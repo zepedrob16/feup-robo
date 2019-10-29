255c3958049c2925107d476d45dde2f146f4bb9f
Jose Borges <up201503603@fe.up.pt>
2019-10-29 17:46:45 +0000

    project structure

---
+++ assignment_2/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bigz/feup-robo/assignment_2/src
+# Build directory: /home/bigz/feup-robo/assignment_2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
